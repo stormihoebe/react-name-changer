93b28adf78b730d6bf6fa9be9d64f8520a7f655b
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-10-04 11:11:30 -0700

    add components directory and Parent component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Parent from './components/Parent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Parent/>
       </div>
     );
   }
